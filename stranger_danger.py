c92e110bf3ffabf415b609fa1508f085c6f29580
Barel Shlidor <Barel@shlidor.org>
2022-02-09 10:47:32 +0200

    Format code

--- stranger_danger.py
+++ stranger_danger.py
@@ -2,38 +2,62 @@
 import math
 
 import cv2
-import imutils
 import numpy as np
 
 # construct the argument parse and parse the arguments
-cv2.namedWindow('trackbars')
 radius_trackbar = 5
 diagonal_aspect = math.hypot(640, 480)
 middle_fov = math.radians(75)
 hor_focal = 640 / (2 * (math.tan(middle_fov / 2) * (640 / diagonal_aspect)))
 ver_focal = 480 / (2 * (math.tan(middle_fov / 2) * (480 / diagonal_aspect)))
+redLower = np.array([4, 108, 52])
+redUpper = np.array([11, 255, 255])
+blueLower = np.array([0, 104, 0])
+blueUpper = np.array([180, 255, 255])
+is_blue = False
 
 
 def callback(_):
     pass
 
 
-def get_pitch(vertical_focal_length, y):
-    return -math.degrees(math.atan((y - 240) / vertical_focal_length))
+def get_pitch(vertical_focal_length, y_pixels):
+    return -math.degrees(math.atan((y_pixels - 240) / vertical_focal_length))
 
 
-def get_yaw(horizontal_focal_length, x):
-    return math.degrees(math.atan((x - 320) / horizontal_focal_length))
+def get_yaw(horizontal_focal_length, x_pixels):
+    return math.degrees(math.atan((x_pixels - 320) / horizontal_focal_length))
 
 
-def get_ratio
+def update_trackbars():
+    global radius_trackbar, is_blue
+
+    radius_trackbar = cv2.getTrackbarPos('radius_trackbars', 'trackbars')
+    is_blue = bool(cv2.getTrackbarPos('is_blue', 'trackbars'))
+
+    if not is_blue:
+        global redLower, redUpper
+        redLower[0] = cv2.getTrackbarPos('min_h', 'trackbars')
+        redLower[1] = cv2.getTrackbarPos('min_s', 'trackbars')
+        redLower[2] = cv2.getTrackbarPos('min_v', 'trackbars')
+
+        redUpper[0] = cv2.getTrackbarPos('max_h', 'trackbars')
+        redUpper[1] = cv2.getTrackbarPos('max_s', 'trackbars')
+        redUpper[2] = cv2.getTrackbarPos('max_v', 'trackbars')
+    else:
+        global blueLower, blueUpper
+        blueLower[0] = cv2.getTrackbarPos('min_h', 'trackbars')
+        blueLower[1] = cv2.getTrackbarPos('min_s', 'trackbars')
+        blueLower[2] = cv2.getTrackbarPos('min_v', 'trackbars')
+
+        blueUpper[0] = cv2.getTrackbarPos('max_h', 'trackbars')
+        blueUpper[1] = cv2.getTrackbarPos('max_s', 'trackbars')
+        blueUpper[2] = cv2.getTrackbarPos('max_v', 'trackbars')
 
 
-redLower = np.array([4, 108, 52])
-redUpper = np.array([11, 255, 255])
-blueLower = (0, 104, 0)
-blueUpper = (180, 255, 255)
+cv2.namedWindow('trackbars')
 
+cv2.createTrackbar('is_blue', 'trackbars', 0, 1, callback)
 cv2.createTrackbar('radius_trackbars', 'trackbars', 0, 200, callback)
 cv2.createTrackbar('min_h', 'trackbars', redLower[0], 180, callback)
 cv2.createTrackbar('min_s', 'trackbars', redLower[1], 255, callback)
@@ -52,23 +76,13 @@ while cv2.waitKey(1) & 0xFF != 27:
 
     if not has_frame:
         print("You are a loser, Why? Because...")
-        break
-
-    radius_trackbar = cv2.getTrackbarPos('radius_trackbars', 'trackbars')
-    redLower[0] = cv2.getTrackbarPos('min_h', 'trackbars')
-    redLower[1] = cv2.getTrackbarPos('min_s', 'trackbars')
-    redLower[2] = cv2.getTrackbarPos('min_v', 'trackbars')
-
-    redUpper[0] = cv2.getTrackbarPos('max_h', 'trackbars')
-    redUpper[1] = cv2.getTrackbarPos('max_s', 'trackbars')
-    redUpper[2] = cv2.getTrackbarPos('max_v', 'trackbars')
+        continue
+    update_trackbars()
 
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-    isBlue = False
-
-    if isBlue:
+    if is_blue:
         mask = cv2.inRange(hsv, blueUpper, blueLower)
     else:
         mask = cv2.inRange(hsv, redLower, redUpper)
@@ -80,31 +94,20 @@ while cv2.waitKey(1) & 0xFF != 27:
 
     cv2.imshow("hsv", cv2.bitwise_and(frame, frame, mask=mask))
 
-    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(contours)
-
-    # hull = cv2.convexHull(contours)
-
-    adam = cv2.Canny(frame, 90, 90 * 3)
-
-    cv2.imshow("Gaia2", adam)
+    contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                   cv2.CHAIN_APPROX_SIMPLE)
 
     # only proceed if at least one contour was found
     if len(contours) > 0:
-        for c in contours:
-            area = cv2.contourArea(c)
+        for contour in contours:
+            area = cv2.contourArea(contour)
             if area < 300:
                 continue
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            if cv2.minEnclosingCircle(c):
-
-            M = cv2.moments(c)
-            center = (int(M["m10"] / (M["m00"] + 1e-54)), int(M["m01"] / (M["m00"] + 1e-54)))
+            (x, y), radius = cv2.minEnclosingCircle(contour)
 
             # only proceed if the radius meets a minimum size
             if radius > radius_trackbar:
-                area = cv2.contourArea(c)
+                area = cv2.contourArea(contour)
                 circle_area = math.pi * radius ** 2
                 solidity = float(area) / circle_area
                 # _, _, w, h = cv2.boundingRect(c)
@@ -113,12 +116,15 @@ while cv2.waitKey(1) & 0xFF != 27:
                 if solidity > 0.6:
                     cv2.circle(frame, (int(x), int(y)), int(radius),
                                (0, 255, 255), 2)
-                    cX = int(M["m10"] / (M["m00"] + 1e-5))
-                    cY = int(M["m01"] / (M["m00"] + 1e-5))
+
+                    M = cv2.moments(contour)
+                    cX = int(M["m10"] / (M["m00"] + 1e-54))
+                    cY = int(M["m01"] / (M["m00"] + 1e-54))
+
                     pitch = get_pitch(ver_focal, cY)
                     yaw = get_yaw(hor_focal, cX)
-                    print("pitch", pitch)
-                    print("yaw", yaw)
+                    cv2.putText(frame, f"pitch: {pitch}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, (209, 80, 0, 255), 3)
+                    cv2.putText(frame, f"yaw: {yaw}", (40, 50), cv2.FONT_HERSHEY_SIMPLEX, (209, 80, 0, 255), 3)
 
     cv2.imshow("frame", frame)
 
